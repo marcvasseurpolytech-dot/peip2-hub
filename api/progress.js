2e3c61787776c3c3ff52932b475aa2e32bb5c2ff
marcvasseurpolytech-dot <marc.vasseur.polytech@gmail.com>
2026-09-18 17:47:42 +0200

    Fix: use CommonJS module.exports (was ESM export default, causing silent failure)

--- api/progress.js
+++ api/progress.js
@@ -4,7 +4,7 @@ const FILE = 'progress.json';
 const TOKEN = process.env.GITHUB_TOKEN;
 const BASE = 'https://api.github.com';
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -37,4 +37,4 @@ export default async function handler(req, res) {
   }
 
   res.status(405).json({ error: 'Method not allowed' });
-}
+};
